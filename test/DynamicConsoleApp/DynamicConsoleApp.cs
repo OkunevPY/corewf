49738f0eddaa6ef541e82adb3297bfd1343b705c
Eric Winnington <eric.in.the.net@gmail.com>
2017-07-19 20:40:13 +0200

    Implemented DynamicActivity for Imperative Code - Exception in DynamicActivites with Properties
    - Added DynamicActivity from ReferenceSource
    - Added DynamicActivityTypeDescriptor.cs from ReferenceSource
    - Created a Sample Test Dynamic that exceptions due to Properties in the DynamicActivity
    - Added DynamicConsoleApp as test ConsoleApp and discovered that DynamicActivities without Properties work correctly.
    
    Reference to the Imperative code used for the sample:
    https://docs.microsoft.com/en-us/dotnet/framework/windows-workflow-foundation/creating-an-activity-at-runtime-with-dynamicactivity

---
+++ test/DynamicConsoleApp/DynamicConsoleApp.cs
@@ -0,0 +1,134 @@
+﻿using CoreWf;
+using CoreWf.Statements;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace DynamicConsoleApp
+{
+    /// <summary>
+    /// Sample DynamicActivity based on https://docs.microsoft.com/en-us/dotnet/framework/windows-workflow-foundation/creating-an-activity-at-runtime-with-dynamicactivity
+    /// </summary>
+    public class DynamicDemo
+    {
+        public static void RunDynamicWriteLine()
+        {
+            //Define the input argument for the activity  
+            var textOut = new InArgument<string>();
+            //Create the activity, property, and implementation  
+            Activity dynamicWorkflow = new DynamicActivity()
+            {
+                Properties =
+                {
+                    new DynamicActivityProperty
+                    {
+                        Name = "Text",
+                        Type = typeof(InArgument<String>),
+                        Value = textOut
+                    }
+                },
+                Implementation = () => new Sequence()
+                {
+                    Activities =
+                    {
+                        new WriteLine()
+                        {
+                            Text = new InArgument<string>(env => textOut.Get(env))
+                        }
+                    }
+                }
+            };
+            //Execute the activity with a parameter dictionary  
+            WorkflowInvoker.Invoke(dynamicWorkflow, new Dictionary<string, object> { { "Text", "Hello World!" } });
+        }
+
+        public static void ParameterlessDelay()
+        {
+            //Define the input argument for the activity  
+            var textOut = new InArgument<string>();
+            //Create the activity, property, and implementation  
+            Activity dynamicWorkflow = new DynamicActivity()
+            {
+                Implementation = () => new Sequence()
+                {
+                    Activities =
+                    {
+                        new Delay()
+                        {
+                            Duration = new InArgument<TimeSpan>(new TimeSpan(200))
+                        }, 
+                        new WriteLine()
+                        {
+                            Text = new InArgument<string>("Delay was successful.")
+                        }
+                    }
+                }
+            };
+            //Execute the activity with a parameter dictionary  
+            WorkflowInvoker.Invoke(dynamicWorkflow);
+        }
+
+        public static void ParameterDelay()
+        {
+            //Define the input argument for the activity  
+            var delayValue = new InArgument<TimeSpan>();
+            //Create the activity, property, and implementation  
+            Activity dynamicWorkflow = new DynamicActivity()
+            {
+                Properties =
+                {
+                    new DynamicActivityProperty
+                    {
+                        Name = "DelayDuration",
+                        Type = typeof(InArgument<TimeSpan>),
+                        Value = delayValue
+                    }
+                },
+                Implementation = () => new Sequence()
+                {
+                    Activities =
+                    {
+                        new Delay()
+                        {
+                            Duration = new InArgument<TimeSpan>(env => delayValue.Get(env))
+                        },
+                        new WriteLine()
+                        {
+                            Text = new InArgument<string>("Delay was successful.")
+                        }
+                    }
+                }
+            };
+            //Execute the activity with a parameter dictionary  
+            WorkflowInvoker.Invoke(dynamicWorkflow, new Dictionary<string, object> { { "DelayDuration", new TimeSpan(200) } });
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------- ParameterlessDelay ------------- ");
+            DynamicDemo.ParameterlessDelay();
+
+            Console.WriteLine("------------- Hello World ------------- ");
+            try
+            {
+                DynamicDemo.RunDynamicWriteLine();
+            } catch (CoreWf.InvalidWorkflowException iwe)
+            {
+                Console.WriteLine("Invalid Workflow WriteLine" + iwe.ToString()); 
+            }
+            Console.WriteLine("------------- ParameterDelay ------------- ");
+            try
+            {
+                DynamicDemo.ParameterDelay();
+            }
+            catch (CoreWf.InvalidWorkflowException iwe)
+            {
+                Console.WriteLine("Invalid Workflow ParameterDelay" + iwe.ToString());
+            }
+            Console.WriteLine("------------- Done ------------- ");
+            Console.ReadLine(); 
+        }
+    }
+}
+
---
+++ test/Samples/Dynamic.cs
@@ -0,0 +1,51 @@
+﻿using CoreWf;
+using CoreWf.Statements;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Samples
+{
+    /// <summary>
+    /// Sample DynamicActivity based on https://docs.microsoft.com/en-us/dotnet/framework/windows-workflow-foundation/creating-an-activity-at-runtime-with-dynamicactivity
+    /// </summary>
+    public class DynamicDemo
+    {
+        [Fact]
+        public void RunDynamicWriteLine()
+        {
+            //Define the input argument for the activity  
+            var textOut = new InArgument<string>();
+            //Create the activity, property, and implementation  
+            Activity dynamicWorkflow = new DynamicActivity()
+            {
+                Properties =
+                {
+                    new DynamicActivityProperty
+                    {
+                        Name = "Text",
+                        Type = typeof(InArgument<String>),
+                        Value = textOut
+                    }
+                },
+                Implementation = () => new Sequence()
+                {
+                    Activities =
+                    {
+                        new WriteLine()
+                        {
+                            Text = new InArgument<string>(env => textOut.Get(env))
+                        }
+                    }
+                }
+            };
+            //Execute the activity with a parameter dictionary  
+            WorkflowInvoker.Invoke(dynamicWorkflow, new Dictionary<string, object> { { "Text", "Hello World!" } });
+        }
+    }
+
+    
+}
