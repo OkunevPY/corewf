7f836cf69d0d58360b6c195dd3f3d18937cebcbb
Eric Winnington <eric.in.the.net@gmail.com>
2018-05-22 11:22:54 +0200

    Reference to local Portable.Xaml with pull request #98 integrated

--- test/XAMLConsoleApp/Program.cs
+++ test/XAMLConsoleApp/Program.cs
@@ -33,6 +33,51 @@ xmlns=""http://schemas.microsoft.com/netfx/2009/xaml/activities""
 xmlns:s=""clr-namespace:System;assembly=mscorlib""  
 xmlns:s1=""clr-namespace:System;assembly=System""  
 xmlns:sa=""clr-namespace:CoreWf;assembly=CoreWf""  
+xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">     
+</Activity>";//<HelloWorldConsole></HelloWorldConsole>  <Sequence></Sequence>
+
+                var act = CoreWf.XamlIntegration.ActivityXamlServices.Load(GenerateStreamFromString(InWriteActivity), settings);
+                WorkflowInvoker.Invoke(act);
+            }
+            catch (Exception ex)
+            { Console.WriteLine(ex.Message.ToString()); }
+            try
+            {
+                string InWriteActivity = @"
+<Activity x:Class=""WFTemplate""  
+xmlns=""http://schemas.microsoft.com/netfx/2009/xaml/activities""  
+xmlns:s=""clr-namespace:System;assembly=mscorlib""  
+xmlns:s1=""clr-namespace:System;assembly=System""  
+xmlns:sa=""clr-namespace:CoreWf;assembly=CoreWf""  
+xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">   
+<x:Members>         
+<x:Property Name=""myInput"" Type=""InArgument(x:String)"" />   
+</x:Members>   
+<HelloWorldConsole Text=""[myInput]"" /> 
+</Activity>";
+                var act = CoreWf.XamlIntegration.ActivityXamlServices.Load(GenerateStreamFromString(InWriteActivity), settings);
+                Dictionary<string, object> dic = new Dictionary<string, object>();
+                dic.Add("myInput", "HelloWorld");
+                WorkflowInvoker.Invoke(act, dic);
+            }
+            catch (Exception ex)
+            { Console.WriteLine(ex.Message.ToString()); }
+
+
+
+
+
+
+
+
+            try
+            {
+                string InWriteActivity = @"
+<Activity x:Class=""WFTemplate""  
+xmlns=""http://schemas.microsoft.com/netfx/2009/xaml/activities""  
+xmlns:s=""clr-namespace:System;assembly=mscorlib""  
+xmlns:s1=""clr-namespace:System;assembly=System""  
+xmlns:sa=""clr-namespace:CoreWf;assembly=CoreWf""  
 xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">   
 <x:Members>         
 <x:Property Name=""myInput"" Type=""InArgument(x:String)"" />   
@@ -43,7 +88,8 @@ xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">
                 Dictionary<string, object> dic = new Dictionary<string, object>();
                 dic.Add("myInput", "HelloWorld");
                 WorkflowInvoker.Invoke(act, dic);
-            } catch (Exception ex)
+            }
+            catch (Exception ex)
             { Console.WriteLine(ex.Message.ToString()); }
             try
             {
@@ -83,107 +129,107 @@ xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">
             }
 
 
-            //            try
-            //            {
-            //                string ActivityAlone = @"
-            //<Activity x:Class=""WFTemplate"" 
-            //xmlns=""http://schemas.microsoft.com/netfx/2009/xaml/activities"" 
-            //xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">   
-            //</Activity>";
-            //                var act = CoreWf.XamlIntegration.ActivityXamlServices.Load(GenerateStreamFromString(ActivityAlone), settings);
-            //                WorkflowInvoker.Invoke(act);
-            //            }
-            //            catch (Exception ex)
-            //            {
-            //                Console.WriteLine(ex.ToString());
-            //            }
-            //            try
-            //            {
-            //                string ActivityWriteLine = @"
-            //<Activity 
-            //x:Class=""WFTemplate"" 
-            //xmlns=""http://schemas.microsoft.com/netfx/2009/xaml/activities"" 
-            //xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">  
-            //<WriteLine Text=""HelloWorld"" /> 
-            //</Activity>";
-            //                var act = CoreWf.XamlIntegration.ActivityXamlServices.Load(GenerateStreamFromString(ActivityWriteLine), settings);
-            //                WorkflowInvoker.Invoke(act);
-            //            }
-            //            catch (Exception ex)
-            //            {
-            //                Console.WriteLine(ex.ToString());
-            //            }
-            //            try
-            //            {
-            //                string InOutActivityOnly = @"
-            //<Activity 
-            //x:Class=""WFTemplate""  
-            //xmlns=""http://schemas.microsoft.com/netfx/2009/xaml/activities""  
-            //xmlns:s=""clr-namespace:System;assembly=mscorlib""  
-            //xmlns:s1=""clr-namespace:System;assembly=System""  
-            //xmlns:sa=""clr-namespace:CoreWf;assembly=CoreWf""  
-            //xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">   
-            //<x:Members>     
-            //<x:Property Name=""myOutput"" Type=""OutArgument(x:Int32)"" />     
-            //<x:Property Name=""myInput"" Type=""InArgument(x:Int32)"" />   
-            //</x:Members> </Activity>";
-            //                var act = CoreWf.XamlIntegration.ActivityXamlServices.Load(GenerateStreamFromString(InOutActivityOnly), settings);
-            //                Dictionary<string, object> dic = new Dictionary<string, object>();
-            //                dic.Add("myInput", 1);
-            //                Dictionary<string, object> outputDict = WorkflowInvoker.Invoke(act, dic) as Dictionary<string, object>;
-            //                foreach (var kvp in outputDict.ToList())
-            //                {
-            //                    Console.WriteLine(kvp.Key.ToString() + " " + kvp.Value.ToString());
-            //                }
-            //            }
-            //            catch (Exception ex)
-            //            {
-            //                Console.WriteLine(ex.ToString());
-            //            }
-            //            try
-            //            {
-            //                string InOutActivity = @"
-            //<Activity x:Class=""WFTemplate""  
-            //xmlns=""http://schemas.microsoft.com/netfx/2009/xaml/activities""  
-            //xmlns:s=""clr-namespace:System;assembly=mscorlib""  
-            //xmlns:s1=""clr-namespace:System;assembly=System""  
-            //xmlns:sa=""clr-namespace:CoreWf;assembly=CoreWf""  
-            //xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">   
-            //<x:Members>     
-            //<x:Property Name=""myOutput"" Type=""OutArgument(x:Int32)"" />     
-            //<x:Property Name=""myInput"" Type=""InArgument(x:Int32)"" />   
-            //</x:Members>   
-            //<Assign>     
-            //<Assign.To> <OutArgument x:TypeArguments=""x:Int32"">[myOutput]</OutArgument> </Assign.To>     
-            //<Assign.Value>       <InArgument x:TypeArguments=""x:Int32"">[myInput]</InArgument>     </Assign.Value>   
-            //</Assign> 
-            //</Activity>";
-            //                var act = CoreWf.XamlIntegration.ActivityXamlServices.Load(GenerateStreamFromString(InOutActivity), settings);
-            //                var outputDict = WorkflowInvoker.Invoke(act) as Dictionary<string, object>;
-            //                foreach (var kvp in outputDict)
-            //                {
-            //                    Console.WriteLine(kvp.Key.ToString() + " " + kvp.Value.ToString());
-            //                }
-            //            }
-            //            catch (Exception ex)
-            //            {
-            //                Console.WriteLine(ex.ToString());
-            //            }
-            //            try
-            //            {
-            //                string XamlText = @"
-            //<Activity x:Class=""WFTemplate"" 
-            //xmlns=""http://schemas.microsoft.com/netfx/2009/xaml/activities"" 
-            //xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">   
-            //<Sequence>     
-            //<WriteLine Text=""HelloWorld"" />   </Sequence> </Activity>";
-            //                var act = CoreWf.XamlIntegration.ActivityXamlServices.Load(GenerateStreamFromString(XamlText), settings);
-            //                WorkflowInvoker.Invoke(act);
-            //            }
-            //            catch (Exception ex)
-            //            {
-            //                Console.WriteLine(ex.ToString());
-            //            }
+            try
+            {
+                string ActivityAlone = @"
+            <Activity x:Class=""WFTemplate"" 
+            xmlns=""http://schemas.microsoft.com/netfx/2009/xaml/activities"" 
+            xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">   
+            </Activity>";
+                var act = CoreWf.XamlIntegration.ActivityXamlServices.Load(GenerateStreamFromString(ActivityAlone), settings);
+                WorkflowInvoker.Invoke(act);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            try
+            {
+                string ActivityWriteLine = @"
+            <Activity 
+            x:Class=""WFTemplate"" 
+            xmlns=""http://schemas.microsoft.com/netfx/2009/xaml/activities"" 
+            xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">  
+            <WriteLine Text=""HelloWorld"" /> 
+            </Activity>";
+                var act = CoreWf.XamlIntegration.ActivityXamlServices.Load(GenerateStreamFromString(ActivityWriteLine), settings);
+                WorkflowInvoker.Invoke(act);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            try
+            {
+                string InOutActivityOnly = @"
+            <Activity 
+            x:Class=""WFTemplate""  
+            xmlns=""http://schemas.microsoft.com/netfx/2009/xaml/activities""  
+            xmlns:s=""clr-namespace:System;assembly=mscorlib""  
+            xmlns:s1=""clr-namespace:System;assembly=System""  
+            xmlns:sa=""clr-namespace:CoreWf;assembly=CoreWf""  
+            xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">   
+            <x:Members>     
+            <x:Property Name=""myOutput"" Type=""OutArgument(x:Int32)"" />     
+            <x:Property Name=""myInput"" Type=""InArgument(x:Int32)"" />   
+            </x:Members> </Activity>";
+                var act = CoreWf.XamlIntegration.ActivityXamlServices.Load(GenerateStreamFromString(InOutActivityOnly), settings);
+                Dictionary<string, object> dic = new Dictionary<string, object>();
+                dic.Add("myInput", 1);
+                Dictionary<string, object> outputDict = WorkflowInvoker.Invoke(act, dic) as Dictionary<string, object>;
+                foreach (var kvp in outputDict.ToList())
+                {
+                    Console.WriteLine(kvp.Key.ToString() + " " + kvp.Value.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            try
+            {
+                string InOutActivity = @"
+            <Activity x:Class=""WFTemplate""  
+            xmlns=""http://schemas.microsoft.com/netfx/2009/xaml/activities""  
+            xmlns:s=""clr-namespace:System;assembly=mscorlib""  
+            xmlns:s1=""clr-namespace:System;assembly=System""  
+            xmlns:sa=""clr-namespace:CoreWf;assembly=CoreWf""  
+            xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">   
+            <x:Members>     
+            <x:Property Name=""myOutput"" Type=""OutArgument(x:Int32)"" />     
+            <x:Property Name=""myInput"" Type=""InArgument(x:Int32)"" />   
+            </x:Members>   
+            <Assign>     
+            <Assign.To> <OutArgument x:TypeArguments=""x:Int32"">[myOutput]</OutArgument> </Assign.To>     
+            <Assign.Value>       <InArgument x:TypeArguments=""x:Int32"">[myInput]</InArgument>     </Assign.Value>   
+            </Assign> 
+            </Activity>";
+                var act = CoreWf.XamlIntegration.ActivityXamlServices.Load(GenerateStreamFromString(InOutActivity), settings);
+                var outputDict = WorkflowInvoker.Invoke(act) as Dictionary<string, object>;
+                foreach (var kvp in outputDict)
+                {
+                    Console.WriteLine(kvp.Key.ToString() + " " + kvp.Value.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            try
+            {
+                string XamlText = @"
+            <Activity x:Class=""WFTemplate"" 
+            xmlns=""http://schemas.microsoft.com/netfx/2009/xaml/activities"" 
+            xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">   
+            <Sequence>     
+            <WriteLine Text=""HelloWorld"" />   </Sequence> </Activity>";
+                var act = CoreWf.XamlIntegration.ActivityXamlServices.Load(GenerateStreamFromString(XamlText), settings);
+                WorkflowInvoker.Invoke(act);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
